a6ff5d55fd11da3676e26401704e12cc6e95748c
wjdguswns123 <wjdguswns123@hanmail.net>
2020-04-24 03:05:04 +0900

    프로젝트 등록

---
+++ Assets/Scripts/GamaManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamaManager : MonoBehaviour
+{
+    public ResourceManager m_ResourceMgr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //m_ResourceMgr.LoadResource("longboard_0");
+        //m_ResourceMgr.LoadResources("cosmo");
+        m_ResourceMgr.LoadResourceInitPos("longboard_0", x : 2f, z : 5f);
+        m_ResourceMgr.LoadResource("longboard_0");
+    }
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    //Object Pool.
+    Dictionary<string, Object> m_ObjectPool = new Dictionary<string, Object>();
+
+    //단일 프리팹 위치 지정 로딩.
+    public GameObject LoadResourceInitPos(string path, float x = 0, float y = 0, float z = 0)
+    {
+        GameObject go = LoadResource(path);
+
+        Vector3 pos = go.transform.localPosition;
+        pos.x = x;
+        pos.y = y;
+        pos.z = z;
+        go.transform.localPosition = pos;
+        go.transform.localRotation = Quaternion.identity;
+        go.transform.localScale = Vector3.one;
+
+        return go;
+    }
+
+    //단일 프리팹 로딩.
+    public GameObject LoadResource(string path)
+    {
+        Object obj = null;
+
+        if(m_ObjectPool.ContainsKey(path))
+        {
+            obj = m_ObjectPool[path];
+        }
+        else
+        {
+            obj = Resources.Load(path);
+            m_ObjectPool.Add(path, obj);
+        }
+        
+        return Instantiate(obj) as GameObject;
+    }
+
+    //폴더 내 모든 프리팹 로딩.
+    public void LoadResources(string path)
+    {
+        Object[] objs = Resources.LoadAll(path);
+        foreach(Object o in objs)
+        {
+            if(!m_ObjectPool.ContainsValue(o))
+            {
+                m_ObjectPool.Add(path + o.name, o);
+            }
+
+            Instantiate(o);
+        }
+    }
+}
