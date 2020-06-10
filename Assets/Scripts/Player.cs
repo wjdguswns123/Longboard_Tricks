5152c286e20bcadb4456dfbc1f4ef1516c8985a2
wjdguswns123 <wjdguswns123@hanmail.net>
2020-06-10 12:48:27 +0900

    Longboarder.FBX Idle, PushOff, Riding 애니메이션 추가. Longboarder 프리팹, 애니메이터 추가.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator            playerAnimator;
+    CharacterController playerController;
+
+    bool                isStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(playerAnimator == null)
+        {
+            playerAnimator = GetComponent<Animator>();
+        }
+
+        if(playerController == null)
+        {
+            playerController = GetComponent<CharacterController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            playerAnimator.SetTrigger("StartRiding");
+            isStart = true;
+        }
+
+        if(isStart)
+        {
+            Vector3 moveValue = transform.forward * 0.01f;
+            moveValue.y = 0f;
+            playerController.Move(moveValue);
+        }
+    }
+}
