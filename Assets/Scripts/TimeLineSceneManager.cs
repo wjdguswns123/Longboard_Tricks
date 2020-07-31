0f47b70d716f21f2200d9946b33441b82d4ca7d2
wjdguswns123 <wjdguswns123@hanmail.net>
2020-07-31 11:00:29 +0900

    - Longboarder 모델링의 180Step 애니메이션 추가. - 180Step 타임라인 추가. - 타임라인 묶음 프리팹화.

---
+++ Assets/Scripts/TimeLineSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLineSceneManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TimeLineController ctrl = ResourceManager.Instance.LoadResource("Pivot").GetComponentInChildren<TimeLineController>();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ResourceManager.Instance.LoadResource("180Step");
+        }
+    }
+}
